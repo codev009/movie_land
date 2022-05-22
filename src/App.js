2e67a8b5a35fe6c9443ba311cb978321324b3e13
codev009 <antoney007i@gmail.com>
2022-05-23 01:52:47 +0530

    add app.ss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { useEffect } from "react";
 
-const API URL = 'http://www.omdbapi.com/?apikey=ed4ff5d2';
+const API_URL = 'http://www.omdbapi.com/?apikey=ed4ff5d2';
 
 const App = () => {
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
+    console.log(data.Search);
+}
+
+useEffect(() => {
+    searchMovies('star wars');
+}, []);
+
+return (
+    <h1>App</h1>
+)
 }
 
 export default App;
