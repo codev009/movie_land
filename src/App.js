0837f07a8e352eb332790ac797ab3fa405fe09a5
codev009 <antoney007i@gmail.com>
2022-05-23 03:03:58 +0530

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+import MoiveCard from "./MovieCard";
 
 import './App.css';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com/?apikey=ed4ff5d2';
 
+const movie1 = {
+        "Title": "Star Wars: Episode IX - The Rise of Skywalker",
+        "Year": "2019",
+        "imdbID": "tt2527338",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
+}
+
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
 }
 
 useEffect(() => {
@@ -25,8 +37,27 @@ return (
        <div className="search">
            <input
                placeholder="Search for movies"
+               value={searchTerm}
+               onChange={(e) => setSearchTerm(e.target.value)}
            />
+           <img src={SearchIcon}
+                alt="search"
+                onClick={() => searchMovies(searchTerm)}
+                />
        </div>
+
+       {movies?.length > 0
+           ? (
+               <div className="container">
+                   {movies.map((movie) => (
+                       <MoiveCard movie={movie}/>
+                   ))}
+               </div>
+           ) : (
+               <div className="empty">
+                   <h2>No movies found</h2>
+               </div>
+           )}
    </div>
 );
 }
